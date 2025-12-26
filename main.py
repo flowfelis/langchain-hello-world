307c0a16ee211e064841dab3138dfac94b060a2e
flowfelis <alicandonmez90@gmail.com>
2025-12-26 18:16:56 +0100

    use gpt-5-mini

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from decouple import config
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
 
 
 def main():
-    print("Hello from langchain-hello-world!")
-    api_key = config("OPENAI_API_KEY")
-    print(api_key)
+    information = """
+        Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2021; as of December 2025, Forbes estimates his net worth to be around $754 billion
+        """
+
+    summary_template = """       
+        given the information {information} about a person I want you to create:
+        1. A short summary
+        2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+    )
+
+    llm = ChatOpenAI(
+        temperature=0,
+        model="gpt-5-mini",
+    )
+
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information})
+
+    print(response.content)
 
 
 if __name__ == "__main__":
