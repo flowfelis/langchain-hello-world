0b112fa2e355cc189dd9135a92328aa3fabaa1b4
flowfelis <alicandonmez90@gmail.com>
2026-01-02 00:21:45 +0100

    add pydantic for a structured response

--- web-search.py
+++ web-search.py
@@ -6,11 +6,26 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
+
+
+class Source(BaseModel):
+    """Schema for url source"""
+
+    url: str = Field(description="URL string for job application link")
+
+
+class AgentResponse(BaseModel):
+    answer: str = Field(description="Agent's response to query")
+    sources: list[Source] = Field(
+        default_factory=list,
+        description="List of sources that shows urls for job applications",
+    )
 
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 if __name__ == "__main__":
     print("starting the web search")
