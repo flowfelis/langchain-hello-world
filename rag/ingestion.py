d4143f7af43a10fac265a321d6bca350118918b9
flowfelis <alicandonmez90@gmail.com>
2026-01-23 11:02:18 +0100

    add vectors to pinecone

--- rag/ingestion.py
+++ rag/ingestion.py
@@ -1,12 +1,29 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core import embeddings
 
 load_dotenv()
 
 from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 from langchain_text_splitters import CharacterTextSplitter
 
 if __name__ == "__main__":
     print("Ingesting...")
-    print(f"PINECONE_API_KEY: {os.getenv('PINECONE_API_KEY')}")
+    loader = TextLoader("/home/puma/Projects/langchain-hello-world/rag/mediumblog1.txt")
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings()
+
+    print("Ingesting...")
+    PineconeVectorStore.from_documents(
+        documents=texts, embedding=embeddings, index_name=os.getenv("INDEX_NAME")
+    )
+    print("finish")
