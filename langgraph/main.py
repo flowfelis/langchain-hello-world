d215564144a9b83962b2271821efa84fa06c7e80
flowfelis <alicandonmez90@gmail.com>
2026-02-20 22:19:18 +0100

    finish langgraph course

--- langgraph/main.py
+++ langgraph/main.py
@@ -17,17 +17,29 @@ def should_continue(state: MessagesState) -> str:
 
 
 flow = StateGraph(MessagesState)
+
 flow.add_node(AGENT_REASON, run_agent_reasoning)
 flow.set_entry_point(AGENT_REASON)
 flow.add_node(ACT, tool_node)
 
-flow.add_edge(AGENT_REASON, ACT)
-flow.add_conditional_edges(AGENT_REASON, should_continue, {END: END, ACT: ACT})
+flow.add_conditional_edges(
+    AGENT_REASON, should_continue, {END: END, ACT: ACT}
+)
 
 flow.add_edge(ACT, AGENT_REASON)
 
 app = flow.compile()
-app.get_graph().draw_mermaid_png(output_file_path="langgraph/flow.png")
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 
 if __name__ == "__main__":
-    print("Hello ReAct LangGraph with Function Calling!")
+    print("Hello ReAct LangGraph with Function Calling")
+    res = app.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="What is the temperature in Tokyo? List it and then triple it"
+                )
+            ]
+        }
+    )
+    print(res["messages"][LAST].content)
--- langgraph/react.py
+++ langgraph/react.py
@@ -5,15 +5,14 @@ from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-
 @tool
-def triple(num: float) -> float:
+def triple(num:float) -> float:
     """
-    param num: A number to be tripled.
-    returns: the triple of a number.
+    param num: a number to triple
+    returns: the triple of the input number
     """
     return float(num) * 3
 
-tools = [triple, TavilySearch(max_results=1)]
+tools = [TavilySearch(max_results=1), triple]
 
-llm = ChatOpenAI(model='gpt-4o-mini', temperature=0).bind_tools(tools)
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
